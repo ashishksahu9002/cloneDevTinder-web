d8522168b0e4f34ee44ee2394da02af70ef9d3de
Ashish Kumar <ashishksahu9002@gmail.com>
2025-10-13 23:56:33 +0530

    Login Component

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">DevTinder</a>
       </div>
       <div className="flex-none gap-2">
         <div className="form-control">
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [emailId, setEmailId] = useState("ravi@gmail.com");
+  const [password, setPassword] = useState("Ravi@123");
 
-export default Login
+  const handleLoginSubmit = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:7777/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center mt-6">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title flex justify-center">Login</h2>
+          <label className="form-control w-full max-w-xs my-2">
+            <div className="label">
+              <span className="label-text">Email</span>
+            </div>
+            <input
+              type="text"
+              value={emailId}
+              className="input input-bordered w-full max-w-xs"
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+          </label>
+          <label className="form-control w-full max-w-xs my-2">
+            <div className="label">
+              <span className="label-text">Password</span>
+            </div>
+            <input
+              type="text"
+              value={password}
+              className="input input-bordered w-full max-w-xs"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLoginSubmit}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
